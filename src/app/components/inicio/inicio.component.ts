975a1038494590350406d5cafbb7d922001a02fb
Carlostorres8791 <escolta87@hotmail.com>
2025-08-18 19:50:20 -0500

    correcion presentación

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,13 +1,16 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgStyle],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  backgroundUrl = './assets/galeria/fachada2.jpg';
 
 }
